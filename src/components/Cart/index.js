457587fdc31a8d3bfce7b5f2cff104f3bf6346c0
yuya-maeda <yuya_m@strmgmt.biz>
2021-10-05 16:04:34 +0900

    fix total price 0

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -3,23 +3,25 @@ import { CartWrapper } from './style';
 import { FaShoppingCart } from 'react-icons/fa';
 import CartContext from '../../context/CartContext';
 
-
-
 export function Cart() {
   const { checkout } = React.useContext(CartContext);
   console.log(checkout, 'checkout');
   let totalQuantity = 0;
+  let totalPrice = 0;
   if (checkout) {
     checkout.lineItems.forEach(lineItem => {
       totalQuantity = totalQuantity + lineItem.quantity;
     });
+    totalPrice = (checkout?.totalPrice * 1).toLocaleString('en-US');
+  } else {
   }
   return (
     <CartWrapper>
-      <FaShoppingCart size="1.5em"  />
+      <FaShoppingCart size="1.5em" />
       <div>
         {totalQuantity} item / ¥
-        {(checkout?.totalPrice *1).toLocaleString('en-US') || 0}
+        {/* {(checkout?.totalPrice * 1).toLocaleString('en-US') || 0} */}
+        {totalPrice}
       </div>
     </CartWrapper>
   );
